64ffce3d7f0240aca7f8b24ce0af78e3f4302bbc
Nagy Attila <nagyatka@gmail.com>
2020-12-31 12:15:39 +0100

    Updated test cases

--- src/PHPDataFrame/PD.php
+++ src/PHPDataFrame/PD.php
@@ -4,8 +4,11 @@
 namespace PHPDataFrame;
 
 
+use PDO;
 use PhpOffice\PhpSpreadsheet\Exception;
 use PhpOffice\PhpSpreadsheet\Reader\Csv;
+use PhpOffice\PhpSpreadsheet\Reader\Xls;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 class PD
@@ -27,12 +30,57 @@ class PD
         return self::processObj($reader->load($filepath_or_buffer), $header_names, $index_col);
     }
 
-    public static function read_xls($file_path) {
-
+    /**
+     * @param $filepath_or_buffer
+     * @param array|null $header_names
+     * @param int|bool $index_col
+     * @return DataFrame
+     * @throws Exception
+     */
+    public static function read_xls($filepath_or_buffer, $header_names=null, $index_col=false) {
+        $reader = new Xls();
+        return self::processObj($reader->load($filepath_or_buffer), $header_names, $index_col);
     }
 
-    public static function read_sql($file_path) {
+    /**
+     * @param $filepath_or_buffer
+     * @param array|null $header_names
+     * @param int|bool $index_col
+     * @return DataFrame
+     * @throws Exception
+     */
+    public static function read_xlsx($filepath_or_buffer, $header_names=null, $index_col=false) {
+        $reader = new Xlsx();
+        return self::processObj($reader->load($filepath_or_buffer), $header_names, $index_col);
+    }
 
+    /**
+     * @param string $sql
+     * @param PDO $pdo
+     * @param array $parameters
+     * @param array|null $header_names
+     * @param int|bool $index_col
+     * @return DataFrame
+     */
+    public static function read_sql($sql, $pdo, $parameters = [], $header_names=null, $index_col=false) {
+        $stmt = $pdo->prepare($sql);
+        foreach ($parameters as $key => $value) {
+            $stmt->bindValue($key, $value);
+        }
+
+        if (!$stmt->execute()) {
+            throw new \RuntimeException("Error occurred during the execution of sql query. PDO error code: ".$pdo->errorCode());
+        }
+
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $columns = $header_names == null ? array_keys($data[0]) : $header_names;
+
+        if($index_col !== false) {
+            $indices = [];
+        }
+
+        return new DataFrame($data, $columns);
     }
 
     /**
@@ -40,7 +88,7 @@ class PD
      *
      * @param Spreadsheet $objWorksheet
      * @param array|null $header_names
-     * @param bool $index_col
+     * @param int|bool $index_col
      * @return DataFrame
      * @throws Exception
      */
@@ -56,10 +104,11 @@ class PD
             $columns  = array_values($objWorksheet->rangeToArray(
                 'A1:'.$highestColumn.'1', null, true,
                 true, true)[1]);
+            $columns = array_filter($columns, function ($values){return !is_null($values);});
             $start_row = 2;
         }
 
-        if($index_col !== false) {
+        if($index_col !== false && $header_names === null) {
             array_splice($columns, $index_col, 1);
         }
 
@@ -71,6 +120,7 @@ class PD
                 true, true, true);
             if ((isset($dataRow[$row]['A'])) && ($dataRow[$row]['A'] > '')) {
                 $row_values =  array_values($dataRow[$row]);
+                $row_values = array_filter($row_values, function ($values){return !is_null($values);});
                 if($index_col !== false) {
                     $indices[] = $row_values[$index_col];
                     array_splice($row_values, $index_col, 1);
@@ -78,7 +128,6 @@ class PD
                 $data[] = array_combine($columns, $row_values);
             }
         }
-        // TODO: indices
         return new DataFrame($data, $columns, $indices);
     }
 }
--- tests/PHPDataFrame/PD/PDTest.php
+++ tests/PHPDataFrame/PD/PDTest.php
@@ -47,4 +47,84 @@ class PDTest extends TestCase
             print($e->getTraceAsString());
         }
     }
+
+    public function testSqlException()
+    {
+        $this->expectException(RuntimeException::class);
+        $stmtMock = $this->createMock(\PDOStatement::class);
+        $pdoMock = $this->createMock(\PDO::class);
+
+        $stmtMock->method('execute')
+            ->willReturn(false);
+        $pdoMock->method('prepare')
+            ->willReturn($stmtMock);
+
+        $df = PD::read_sql("SELECT * FROM table_name", $pdoMock);
+    }
+
+    public function testSqlWithResults()
+    {
+        $stmtMock = $this->createMock(\PDOStatement::class);
+        $pdoMock = $this->createMock(\PDO::class);
+
+        $stmtMock->method('execute')
+            ->willReturn(true);
+        $stmtMock->method('fetchAll')
+            ->willReturn([
+                ["a" => 0, "b" => 1, "c" => 2],
+                ["a" => 3, "b" => 4, "c" => 5],
+                ["a" => 6, "b" => 7, "c" => 8],
+            ]);
+        $pdoMock->method('prepare')
+            ->willReturn($stmtMock);
+
+        $df = PD::read_sql("SELECT * FROM table_name", $pdoMock);
+
+        $this->assertEquals($df->shape, [3, 3]);
+        $this->assertEquals($df->at(0,0), 0);
+        $this->assertEquals($df->at(1,1), 4);
+        $this->assertEquals($df->at(2,2), 8);
+    }
+
+    public function testXlsxWithoutHeader()
+    {
+        try {
+            $df = PD::read_xlsx("test_xls.xlsx");
+            $this->assertEquals($df->shape, [3, 4]);
+            $this->assertEquals($df->at(0,0), "e");
+            $this->assertEquals($df->at(1,1), 4);
+            $this->assertEquals($df->at(2,2), 8);
+            $this->assertEquals(["idx", "a", "b", "c"],  $df->getColumnNames());
+        } catch (\PhpOffice\PhpSpreadsheet\Exception $e) {
+            print($e->getTraceAsString());
+        }
+    }
+
+    public function testXlsxWithoutHeaderIndexCol()
+    {
+        try {
+            $df = PD::read_xlsx("test_xls.xlsx", null, 0);
+            $this->assertEquals($df->shape, [3, 3]);
+            $this->assertEquals($df->at(0,0), 1);
+            $this->assertEquals($df->at(1,1), 5);
+            $this->assertEquals($df->at(2,2), 9);
+            $this->assertEquals(["a", "b", "c"],  $df->getColumnNames());
+        } catch (\PhpOffice\PhpSpreadsheet\Exception $e) {
+            print($e->getTraceAsString());
+        }
+    }
+
+    public function testXlsxWithHeaderIndexCol()
+    {
+        try {
+            $df = PD::read_xlsx("test_xls.xlsx", ["x", "y", "z"], 0);
+            $this->assertEquals($df->shape, [4, 3]);
+            $this->assertEquals($df->at(0,0), "a");
+            $this->assertEquals($df->at(1,1), 2);
+            $this->assertEquals($df->at(2,2), 6);
+            $this->assertEquals(["x", "y", "z"],  $df->getColumnNames());
+        } catch (\PhpOffice\PhpSpreadsheet\Exception $e) {
+            print($e->getTraceAsString());
+        }
+    }
 }
